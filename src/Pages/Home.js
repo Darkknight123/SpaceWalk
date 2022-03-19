572d6d1ea40f8da89ac6cdf41f0ba804cb4a4433
Darkknight123 <philipkerry57@gmail.com>
2022-03-19 13:48:01 +0300

    Done with home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,7 +5,19 @@ import background from '../assets/home/background-home-desktop.jpg'
 
 function Home() {
   return (
-    <div style={{ backgroundImage: `url(${background})` }}>  <Navbar/></div>
+    <div style={{ backgroundImage: `url(${background})` }} className='home'>
+       <Navbar/>
+       <div className='landing'>
+         <div className='left-landing'>
+           <h1 className='travel'>SO, YOU WANT TO TRAVEL TO </h1>
+           <h1 className='space'><b>SPACE</b></h1>
+           <p className='lets'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+         </div>
+         <div className='right-landing'>
+           <button className='explore'>EXPLORE</button>
+         </div>
+       </div>
+       </div>
   
   )
 }
